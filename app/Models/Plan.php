e4710cf503c5a341b2e9e18f775f4e066fdd82a3
Mattia-Fasulo <mattia.fasulo@gmail.com>
2023-02-06 14:49:13 +0100

    add plan model

---
+++ app/Models/Plan.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Plan extends Model
+{
+    use HasFactory;
+
+    public function professionists():BelongsToMany{
+        return $this->belongsToMany(Professionist::class)->withPivot('subscription_start','subscription_end');
+    }
+}
+
+
