2c94f7577643b723a92772db068f5e0345258e34
Mattia-Giorgetti <mattiagiorgetti37@gmail.com>
2023-02-06 12:05:34 +0100

    config add

---
+++ config/tech_in_touch_seeder.php
@@ -0,0 +1,172 @@
+<?php
+
+return [
+    'professionists' => [
+        [
+            "nickname" => "Fasulino",
+            "job_address" => "123 Anywhere St., Potenza",
+            "profile_image" => "https://media.licdn.com/dms/image/D4D03AQHRxgN2NdfHFQ/profile-displayphoto-shrink_800_800/0/1675169063591?e=1680739200&v=beta&t=0FPPCQwp9HpE5ZTKlQ_LDaZwyl2n16vPvDczuW1IqqQ",
+            "description" => "La comunicazione è il mio punto di forza.
+            Lavoro nel settore da oltre 10 anni, maturando esperienza, contatti e capacità di gestione dei clienti. Lavorando in aziende appartenenti a settori diversi, ho imparato ad essere flessibile e promuovere prodotti e servizi a 360°, sia online che offline.
+            Ho vissuto all'estero due anni, perfezionando il mio inglese e raccogliendo esperienza in grandi agenzie di comunicazione tessendo relazioni internazionali.",
+            "phone_number" => "+123-456-7890",
+            "cv_link" => "foto_cv.jpg",
+            "linkedin_page" => "https://www.linkedin.com/in/mattia-fasulo/",
+            "github_page" => "https://github.com/Mattia-Fasulo",
+            "average_rate" => ""
+        ],
+    ],
+    // 'user_types' => [
+    //     [
+    //         "name" => "registered"
+    //     ],
+    //     [
+    //         "name" => "professionist"
+    //     ],
+    //     [
+    //         "name" => "admin"
+    //     ]
+    // ],
+    // 'job_titles' => [
+    //     [
+    //         "name" => "Full Stack Developer"
+    //     ],
+    //     [
+    //         "name" => "Front End Developer"
+    //     ],
+    //     [
+    //         "name" => "Back End Developer"
+    //     ],
+    //     [
+    //         "name" => "UI/UX Designer"
+    //     ]
+    // ],
+    // 'work_performances' => [
+    //     [
+    //         "name" => "Consulenza"
+    //     ],
+    //     [
+    //         "name" => "Lezioni Private"
+    //     ],
+    //     [
+    //         "name" => "Freelancer"
+    //     ]
+    // ],
+    'technologies' => [
+        [
+            "name" => "HTML",
+            "icon" => '<i class="fa-brands fa-html5"></i>'
+        ],
+        [
+            "name" => "CSS",
+            "icon" => '<i class="fa-brands fa-css3-alt"></i>'
+        ],
+        [
+            "name" => "Sass",
+            "icon" => '<i class="fa-brands fa-sass"></i>'
+        ],
+        [
+            "name" => "Bootstrap",
+            "icon" => '<i class="fa-brands fa-bootstrap"></i>'
+        ],
+        [
+            "name" => "JavaScript",
+            "icon" => '<i class="fa-brands fa-js"></i>'
+        ],
+        [
+            "name" => "Java",
+            "icon" => '<i class="fa-brands fa-java"></i>'
+        ],
+        [
+            "name" => "Python",
+            "icon" => '<i class="fa-brands fa-python"></i>'
+        ],
+        [
+            "name" => "C",
+            "icon" => '<i class="fa-solid fa-c"></i>'
+        ],
+        [
+            "name" => "PHP",
+            "icon" => '<i class="fa-brands fa-php"></i>'
+        ],
+        [
+            "name" => "SQL",
+            "icon" => '<i class="fa-solid fa-database"></i>'
+        ],
+        [
+            "name" => "React JS",
+            "icon" => '<i class="fa-brands fa-react"></i>'
+        ],
+        [
+            "name" => "Vue JS",
+            "icon" => '<i class="fa-brands fa-vuejs"></i>'
+        ],
+        [
+            "name" => "Angular",
+            "icon" => '<i class="fa-brands fa-angular"></i>'
+        ],
+        [
+            "name" => "Laravel",
+            "icon" => '<i class="fa-brands fa-laravel"></i>'
+        ],
+        [
+            "name" => "Flask",
+            "icon" => '<i class="fa-solid fa-pepper-hot"></i>'
+        ],
+        [
+            "name" => "Golang",
+            "icon" => '<i class="fa-brands fa-golang"></i>'
+        ],
+        [
+            "name" => "Adobe Photoshop",
+            "icon" => '<i class="fi fi-brands-photoshop"></i>'
+        ],
+        [
+            "name" => "Adobe Illustrator",
+            "icon" => '<i class="fi fi-brands-illustrator"></i>'
+        ],
+        [
+            "name" => "Figma",
+            "icon" => '<i class="fa-brands fa-figma"></i>'
+        ],
+        [
+            "name" => "Adobe XD",
+            "icon" => '<i class="fi fi-brands-xd"></i>'
+        ],
+        [
+            "name" => "Sketch",
+            "icon" => '<i class="fa-brands fa-sketch"></i>'
+        ],
+        [
+            "name" => "InVision",
+            "icon" => '<i class="fa-brands fa-invision"></i>'
+        ],
+    ],
+    'plans' => [
+        [
+            "name" => "Standard",
+            "price" => "2,99",
+            "price_sign" => "€",
+            "duration" => "1"
+        ],
+        [
+            "name" => "Plus",
+            "price" => "5,99",
+            "price_sign" => "€",
+            "duration" => "2"
+        ],
+        [
+            "name" => "Ultra",
+            "price" => "9,99",
+            "price_sign" => "€",
+            "duration" => "3"
+        ],
+    ],
+    'projects' => [
+        [
+            "name" => "Spotify Web",
+            "description" => "Riproduzione del layout in HTML/CSS di Spotify",
+            "cover_image" => "https://s3-us-west-1.amazonaws.com/coppertino/seo-photos/spotify-web-player-vs-loop-web.jpg"
+        ]
+    ],
+];
