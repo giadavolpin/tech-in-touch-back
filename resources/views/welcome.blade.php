7949280fe5e4f02bdc20d16f85a8f7aea2c5bf8b
StefaniaSgreva <stefaniasgreva@gmail.com>
2023-02-08 16:58:59 +0100

    welcome add

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 @section('content')
 <div class="welcome">
-    <h1 class="text-venter">Benvenuti in <br> Tech in Touch</h1>
+    <h1 class="text-venter">Benvenuti in <br> Tech in Touch <br> (Vue)</h1>
 </div>
 @endsection
