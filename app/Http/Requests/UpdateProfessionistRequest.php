8bb6192eeea045ed4eb9d82b9d26953d6d2f1029
Mattia-Giorgetti <mattiagiorgetti37@gmail.com>
2023-02-08 12:26:36 +0100

    fixed required field

--- app/Http/Requests/UpdateProfessionistRequest.php
+++ app/Http/Requests/UpdateProfessionistRequest.php
@@ -26,10 +26,12 @@ class UpdateProfessionistRequest extends FormRequest
     public function rules()
     {
         return [
-            'nickname' => 'nullable',
-            'string',
-            'min:3',
-            'max:15', Rule::unique('professionists')->ignore($this->professionist),
+            'nickname' => [
+                'nullable',
+                'string',
+                'min:3',
+                'max:15', Rule::unique('professionists')->ignore($this->professionist)
+            ],
             'name' => ['required', 'string', 'min:3', 'max: 50'],
             'surname' => ['required', 'string', 'min:3', 'max: 50'],
             'job_address' => ['nullable', 'string', 'min:3', 'max: 50'],
@@ -37,8 +39,27 @@ class UpdateProfessionistRequest extends FormRequest
             'bio' => ['nullable'],
             'profile_image' => ['nullable', 'image'],
             'cv_path' => ['required'],
-            'linkedin' => ['nullable'],
-            'github' => ['nullable']
+            'linkedin' => ['nullable', 'string'],
+            'github' => ['nullable', 'string']
+        ];
+    }
+    public function messages()
+    {
+        return [
+            'nickname.min' => 'Il nickname deve essere almeno :min caratteri',
+            'nickname.max' => 'Il nickname deve essere almeno :max caratteri',
+            'name.required' => 'Compila il campo',
+            'name.min' => 'Il Nome deve essere almeno :min caratteri',
+            'name.max' => 'Il Nome deve essere almeno :max caratteri',
+            'surname.min' => 'Il Cognome deve essere almeno :min caratteri',
+            'surname.max' => 'Il Cognome deve essere almeno :max caratteri',
+            'job_address.min' => 'Indirizzo deve essere almeno :min caratteri',
+            'job_address.max' => 'Indirizzo deve essere almeno :max caratteri',
+            'phone_number.max' => 'Il numero di telefono deve essere massimo :max numeri',
+            'profile_image.image' => 'Il file deve essere una immagine',
+            'cv_path.required' => 'Il campo è obbligatorio',
+            'linkedin.string' => 'Inserisci un URL',
+            'github.string' => 'Inserici un URL'
         ];
     }
 }
