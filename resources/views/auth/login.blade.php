70791f482c0677e5e2467f04604220a584ae6d8c
Mattia-Giorgetti <mattiagiorgetti37@gmail.com>
2023-02-10 10:59:27 +0100

    messaggi errore login/register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,48 +1,62 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="dev_login_full_page">
-    <div class="box_login">
-        
-        <form method="POST" action="{{ route('login') }}" class="form_login" id="login-form">
-            @csrf
-            
-            <h2>{{ __('Login') }} <i class="fa-solid fa-arrow-right-to-bracket"></i></h2>
+    <div class="dev_login_full_page">
+        <div class="auth_errors_div">
+            @if ($errors->any())
+                <div class="auth_errors_list">
+                    <ul class="mb-0">
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+        </div>
+        <div class="box_login">
 
-            <div class="inputBox_login">
-                {{-- <label for="email">{{ __('E-Mail Address') }}</label> --}}
-                <input id="email" type="email" class=" @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+            <form method="POST" action="{{ route('login') }}" class="form_login" id="login-form">
+                @csrf
 
-                @error('email')
+                <h2>{{ __('Login') }} <i class="fa-solid fa-arrow-right-to-bracket"></i></h2>
+
+                <div class="inputBox_login">
+                    {{-- <label for="email">{{ __('E-Mail Address') }}</label> --}}
+                    <input id="email" type="email" class=" @error('email') is-invalid @enderror" name="email"
+                        value="{{ old('email') }}" required autocomplete="email" autofocus>
+
+                    {{-- @error('email')
                 <span class="dev_error_login" role="alert">
                     <strong>{{ $message }}</strong>
                 </span>
-                @enderror
-                
-                <span class="label">Email</span>
-                {{-- <i></i> --}}
-            </div>
+                @enderror --}}
 
-            <div class="inputBox_login">
-                {{-- <label for="password">{{ __('Password') }}</label> --}}
-                <input autocomplete="new-password" id="password" type="password" class="@error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                    <span class="label">Email</span>
+                    {{-- <i></i> --}}
+                </div>
 
-                @error('password')
+                <div class="inputBox_login">
+                    {{-- <label for="password">{{ __('Password') }}</label> --}}
+                    <input autocomplete="new-password" id="password" type="password"
+                        class="@error('password') is-invalid @enderror" name="password" required
+                        autocomplete="current-password">
+
+                    {{-- @error('password')
                 <span class="dev_error_login" role="alert">
                     <strong>{{ $message }}</strong>
                 </span>
-                @enderror
-               
-                <span class="label">Password</span>
-                {{-- <i></i> --}}
-            </div>
+                @enderror --}}
 
-            <div class="links_login">
-                <span>Non hai ancora un account ?</span>
-                <a href="http://127.0.0.1:8000/register">Registrati</a>
-            </div>
-            <button type="submit" class="btn_login">{{ __('Login') }}</button>
-{{-- 
+                    <span class="label">Password</span>
+                    {{-- <i></i> --}}
+                </div>
+
+                <div class="links_login">
+                    <span>Non hai ancora un account ?</span>
+                    <a href="http://127.0.0.1:8000/register">Registrati</a>
+                </div>
+                <button type="submit" class="btn_login">{{ __('Login') }}</button>
+                {{--
             <div class="mb-4 row">
                 <div class="col-md-6 offset-md-4">
                     <div class="form-check">
@@ -55,7 +69,7 @@
                 </div>
             </div> --}}
 
-            {{-- <div class="mb-4 row mb-0">
+                {{-- <div class="mb-4 row mb-0">
                 <div class="col-md-8 offset-md-4">
                     <button type="submit" class="btn btn-primary">
                         {{ __('Login') }}
@@ -68,11 +82,7 @@
                     @endif
                 </div>
             </div> --}}
-        </form>
+            </form>
+        </div>
     </div>
-</div>
-
-
-
 @endsection
-
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,100 +2,107 @@
 
 @section('content')
     <div class="dev_register_full_page">
+        <div class="auth_errors_div">
+            @if ($errors->any())
+                <div class="auth_errors_list">
+                    <ul class="mb-0">
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+        </div>
+
         <div class="box_register">
 
             <form method="POST" action="{{ route('register') }}" class="dev_register_form">
-                
+
                 @csrf
 
                 <h2>{{ __('Registrati') }}</h2>
 
                 <div class="inputBox_register">
                     {{-- <label for="name">{{ __('Name') }}</label> --}}
-                    <input id="name" type="text"
-                    class="@error('name') is-invalid @enderror" name="name"
-                    value="{{ old('name') }}" required autocomplete="name" autofocus>
+                    <input id="name" type="text" class="@error('name') is-invalid @enderror" name="name"
+                        value="{{ old('name') }}" required autocomplete="name" autofocus>
 
-                    @error('name')
+                    {{-- @error('name')
                         <span class="dev_error" role="alert">
                             <strong>{{ $message }}</strong>
                         </span>
-                    @enderror
+                    @enderror --}}
 
-                    <span>Nome</span>
+                    <span><strong style="color: #BB2649">*</strong>Nome</span>
                     <i></i>
                 </div>
 
                 <div class="inputBox_register">
                     {{-- <label for="surname">{{ __('Surname') }}</label> --}}
-                    <input id="surname" type="text"
-                        class="@error('surname') is-invalid @enderror" name="surname"
+                    <input id="surname" type="text" class="@error('surname') is-invalid @enderror" name="surname"
                         value="{{ old('surname') }}" required autocomplete="surname" autofocus>
 
-                    @error('surname')
+                    {{-- @error('surname')
                         <span class="dev_error" role="alert">
                             <strong>{{ $message }}</strong>
                         </span>
-                    @enderror
-                    
-                    <span>Cognome</span>
+                    @enderror --}}
+
+                    <span><strong style="color: #BB2649">*</strong>Cognome</span>
                     <i></i>
                 </div>
 
                 <div class="inputBox_register">
                     {{-- <label for="address">{{ __('Address') }}</label> --}}
-                    <input id="address" type="text"
-                        class="@error('address') is-invalid @enderror" name="address"
+                    <input id="address" type="text" class="@error('address') is-invalid @enderror" name="address"
                         value="{{ old('address') }}" required autocomplete="address" autofocus>
 
-                    @error('address')
+                    {{-- @error('address')
                         <span class="dev_error" role="alert">
                             <strong>{{ $message }}</strong>
                         </span>
-                    @enderror
-                    
-                    <span>Indizizzo</span>
+                    @enderror --}}
+
+                    <span><strong style="color: #BB2649">*</strong>Indizizzo</span>
                     <i></i>
                 </div>
 
                 <div class="inputBox_register">
                     {{-- <label for="email">{{ __('E-Mail Address') }}</label> --}}
-                    <input id="email" type="email"
-                        class="@error('email') is-invalid @enderror" name="email"
+                    <input id="email" type="email" class="@error('email') is-invalid @enderror" name="email"
                         value="{{ old('email') }}" required autocomplete="email">
 
-                    @error('email')
+                    {{-- @error('email')
                         <span class="dev_error" role="alert">
                             <strong>{{ $message }}</strong>
                         </span>
-                    @enderror
+                    @enderror --}}
 
-                    <span>Email</span>
+                    <span><strong style="color: #BB2649">*</strong>Email</span>
                     <i></i>
                 </div>
 
                 <div class="inputBox_register">
                     {{-- <label for="password">{{ __('Password') }}</label> --}}
-                    <input id="password" type="password"
-                        class="@error('password') is-invalid @enderror" name="password"
+                    <input id="password" type="password" class="@error('password') is-invalid @enderror" name="password"
                         required autocomplete="new-password">
 
-                    @error('password')
+                    {{-- @error('password')
                         <span class="dev_error" role="alert">
                             <strong>{{ $message }}</strong>
                         </span>
-                    @enderror
-                   
-                    <span>Password</span>
+                    @enderror --}}
+
+                    <span><strong style="color: #BB2649">*</strong>Password</span>
                     <i></i>
                 </div>
 
                 <div class="inputBox_register">
                     {{-- <label for="password-confirm">{{ __('Confirm Password') }}</label> --}}
-                    <input id="password-confirm" type="password"
-                        name="password_confirmation" required autocomplete="new-password">
+                    <input id="password-confirm" type="password" name="password_confirmation" required
+                        autocomplete="new-password">
 
-                    <span>Conferma Password</span>
+                    <span><strong style="color: #BB2649">*</strong>Conferma Password</span>
                     <i></i>
                 </div>
 
@@ -103,13 +110,13 @@
                     <span>Sei già registrato ?</span>
                     <a href="http://127.0.0.1:8000/login">Accedi</a>
                 </div>
-                
+
                 <button type="submit" class="register">
                     {{ __('Registrati') }}
                 </button>
-                    
-                </div>
-            </form>   
+            </form>
         </div>
+
+    </div>
     </div>
 @endsection
