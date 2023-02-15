7c03caade6ddee2dbaaaee40bd6226f88177e61d
Mattia-Fasulo <mattia.fasulo@gmail.com>
2023-02-15 15:53:24 +0100

    fix user

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,7 @@ namespace App\Models;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -45,8 +46,7 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-    public function professionists(): HasMany
-    {
-        return $this->hasMany(Professionist::class);
+    public function professionist(): HasOne{
+        return $this->hasOne(Professionist::class);
     }
 }
