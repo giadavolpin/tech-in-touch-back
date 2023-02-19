af4bcda8bb0dd8e861d53c546b7ed16af3451a7b
Mattia-Giorgetti <mattiagiorgetti37@gmail.com>
2023-02-19 10:57:40 +0100

    prova js d none

--- resources/views/admin/Braintree/braintree.blade.php
+++ resources/views/admin/Braintree/braintree.blade.php
@@ -9,60 +9,60 @@
 
         {{-- SE È FALSO APPARE QUESTO --}}
         @if (isset($end_date))
-            <h2 class="text-center py-5">A quanto pare hai già un abbonamento attivo</h2>
-            <p class="fs-4 text-center">Il tuo abbonamento <span
-                    class="dev_btn p-1 rounded-2 fw-bold">{{ $plan_name }}</span>
-                scade
-                tra: <span class="mx-1 backoffice_title" id="timer"></span>
-            </p>
-
-            {{-- SE È VERO APPARE QUESTA PER RICOMPRARE UNA SPONSOPRIZZAZIONE --}}
-        @else
-            <h4 class="py-5 text-center">Ecco i piani disponibili</h4>
-            <div class="row gap-4 justify-content-center">
-                <div class="col-lg-3">
-                    <div class="my_card">
-                        <h4>Standard</h4>
-                        <p>Compari in Homepage nella sezione dedicata ai nostri professionisti sponsorizzati </p>
-                        <p>24h di Sponsorizzazione</p>
-                    </div>
-                </div>
-                <div class="col-lg-3">
-                    <div class="my_card">
-                        <h4>Plus</h4>
-                        <p>Compari in Homepage nella sezione dedicata ai nostri professionisti sponsorizzati </p>
-                        <p>48h di Sponsorizzazione</p>
-                    </div>
-                </div>
-                <div class="col-lg-3">
-                    <div class="my_card">
-                        <h4>Ultra</h4>
-                        <p>Compari in Homepage nella sezione dedicata ai nostri professionisti sponsorizzati </p>
-                        <p>72h di Sponsorizzazione</p>
-                    </div>
-                </div>
-
+            <div id="active_sub">
+                <h2 class="text-center py-5">A quanto pare hai già un abbonamento attivo</h2>
+                <p class="fs-4 text-center">Il tuo abbonamento <span
+                        class="dev_btn p-1 rounded-2 fw-bold">{{ $plan_name }}</span>
+                    scade
+                    tra: <span class="mx-1 backoffice_title" id="timer"></span>
+                </p>
             </div>
 
-
-            <div class="form_payment">
-                <h4 class="py-5 text-center">Scegli il piano ed entra a far parte dei nostri <span
-                        class="backoffice_title">Professionisti
-                        Sponsorizzati</span></h4>
-                <form action="{{ route('admin.braintree') }}" method="POST" id="payment-form" class="mt-3">
-                    @csrf
-                    {{-- SELECT PER I PIANI --}}
-                    <select name="plan" id="plan">
-                        @foreach ($plans as $plan)
-                            <option value="{{ $plan->id }}">{{ $plan->name }}</option>
-                        @endforeach
-                        <input type="hidden" id="nonce" name="payment_method_nonce">
-                    </select>
-                    {{-- DIV PER L UI DI BRAINTREE --}}
-                    <div id="dropin-container"></div>
-                    {{-- <button type="submit" id="submit-button" class="button button--small button--green">Purchase</button> --}}
-                    <input type="submit" class="btn dev_btn" />
-                </form>
+            {{-- SE È VERO APPARE QUESTA PER RICOMPRARE UNA SPONSOPRIZZAZIONE --}}
+            <div id="no_active_sub">
+                <h4 class="py-5 text-center">Ecco i piani disponibili</h4>
+                <div class="row gap-4 justify-content-center">
+                    <div class="col-lg-3">
+                        <div class="my_card">
+                            <h4>Standard</h4>
+                            <p>Compari in Homepage nella sezione dedicata ai nostri professionisti sponsorizzati </p>
+                            <p>24h di Sponsorizzazione</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-3">
+                        <div class="my_card">
+                            <h4>Plus</h4>
+                            <p>Compari in Homepage nella sezione dedicata ai nostri professionisti sponsorizzati </p>
+                            <p>48h di Sponsorizzazione</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-3">
+                        <div class="my_card">
+                            <h4>Ultra</h4>
+                            <p>Compari in Homepage nella sezione dedicata ai nostri professionisti sponsorizzati </p>
+                            <p>72h di Sponsorizzazione</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="form_payment">
+                    <h4 class="py-5 text-center">Scegli il piano ed entra a far parte dei nostri <span
+                            class="backoffice_title">Professionisti
+                            Sponsorizzati</span></h4>
+                    <form action="{{ route('admin.braintree') }}" method="POST" id="payment-form" class="mt-3">
+                        @csrf
+                        {{-- SELECT PER I PIANI --}}
+                        <select name="plan" id="plan">
+                            @foreach ($plans as $plan)
+                                <option value="{{ $plan->id }}">{{ $plan->name }}</option>
+                            @endforeach
+                            <input type="hidden" id="nonce" name="payment_method_nonce">
+                        </select>
+                        {{-- DIV PER L UI DI BRAINTREE --}}
+                        <div id="dropin-container"></div>
+                        {{-- <button type="submit" id="submit-button" class="button button--small button--green">Purchase</button> --}}
+                        <input type="submit" class="btn dev_btn" />
+                    </form>
+                </div>
             </div>
     </div>
     @endif
@@ -72,6 +72,8 @@
     <script src="https://js.braintreegateway.com/web/dropin/1.33.7/js/dropin.js"></script>
     <script>
         //=====================   FUNZIONE PER IL TIMER DATE=============================
+        var activeSub = getElementById('active_sub')
+        var no_activeSub = getElementById('no_active_sub')
         var endDate = new Date('{{ $end_date }}')
         var sponsor_end = false;
 
@@ -84,26 +86,27 @@
                 sponsor_end = true;
                 clearInterval(timerInterval);
                 console.log(sponsor_end);
-                fetch('Braintree', {
-                        method: 'POST',
-                        body: 'sponsor_endPHP=' + sponsor_end,
-                        headers: {
-                            'Content-Type': 'application/x-www-form-urlencoded',
-                            'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute(
-                                'content')
-                        }
-                    }).then(response => {
-                        if (response.ok) {
-                            console.log('è andata bene')
-                            console.log(response);
+                activeSub.classList.add('d-none')
+                // fetch('Braintree', {
+                //         method: 'POST',
+                //         body: 'sponsor_endPHP=' + sponsor_end,
+                //         headers: {
+                //             'Content-Type': 'application/x-www-form-urlencoded',
+                //             'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute(
+                //                 'content')
+                //         }
+                //     }).then(response => {
+                //         if (response.ok) {
+                //             console.log('è andata bene')
+                //             console.log(response);
 
-                        } else {
-                            console.log('è andata male')
-                        }
-                    })
-                    .catch(error => {
-                        console.log('errore nella richiesta:', error);
-                    });
+                //         } else {
+                //             console.log('è andata male')
+                //         }
+                //     })
+                //     .catch(error => {
+                //         console.log('errore nella richiesta:', error);
+                //     });
             } else {
                 sponsor_end = false;
                 var diffinHours = Math.floor(timediff / 60);
